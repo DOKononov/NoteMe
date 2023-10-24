eb3747fc02a3df19c633a8d6e44810e60cb9a633
d.o.kononov <d.o.kononov@gmail.com>
2023-10-24 19:44:55 +0300

    Initial Commit

---
+++ NoteMe/VC.swift
@@ -0,0 +1,16 @@
+//
+//  VC.swift
+//  NoteMe
+//
+//  Created by Dmitry Kononov on 24.10.23.
+//
+
+import UIKit
+
+final class VC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
