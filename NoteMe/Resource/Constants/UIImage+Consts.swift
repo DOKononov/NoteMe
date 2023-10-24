96499940ab2d55747193fbc54256f2a944db1003
d.o.kononov <d.o.kononov@gmail.com>
2023-10-24 20:52:13 +0300

    added SnapKit added constance

---
+++ NoteMe/Resource/Constants/UIImage+Consts.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Consts.swift
+//  NoteMe
+//
+//  Created by Dmitry Kononov on 24.10.23.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    enum General {
+        static let logo: UIImage = .init(named: "logo")!
+        
+    }
+    
+}
