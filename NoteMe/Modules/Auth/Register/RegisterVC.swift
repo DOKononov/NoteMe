42126c4040cc7e6726eacb11e1a1597fe735f0f6
DOKononov <84906625+DOKononov@users.noreply.github.com>
2023-11-02 13:10:12 +0300

    done (#2)
    add Register Module UI
    add extsn UIViewController+EndEditing

---
+++ NoteMe/Modules/Auth/Register/RegisterVC.swift
@@ -0,0 +1,119 @@
+//
+//  RegisterVC.swift
+//  NoteMe
+//
+//  Created by Dmitry Kononov on 2.11.23.
+//
+
+import UIKit
+import SnapKit
+
+final class RegisterVC: UIViewController {
+    
+    private lazy var contentView: UIView = .contentView()
+    private lazy var logoImageView: UIImageView = .init(image: .General.logo)
+    private lazy var welcomeLabel: UILabel = .titleLabel(.LoginVC.niceToMeetYou)
+    private lazy var infoView: UIView = .infoView()
+    
+    private lazy var emailTextView: LineTextField = {
+        let view = LineTextField()
+        view.title = .LoginVC.email
+        view.placeholder = .LoginVC.enterEmail
+        return view
+    }()
+    
+    private lazy var passwordTextView: PasswordLineTextField = {
+        let view = PasswordLineTextField()
+        view.title = .LoginVC.password
+        view.placeholder = .LoginVC.enterPassword
+        return view
+    }()
+    
+    private lazy var repeatPasswordTextView: PasswordLineTextField = {
+        let view = PasswordLineTextField()
+        view.title = .LoginVC.repeatPassword
+        view.placeholder = .LoginVC.enterPassword
+        return view
+    }()
+    
+    private lazy var registerButton: UIButton = .yellowRoundedButton(.LoginVC.register)
+    private lazy var iHaveAnAccountButton: UIButton = .underlineYellowButton(.LoginVC.iHaveAnAccount)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupLayouts()
+    }
+}
+
+//MARK: -setupUI
+private extension RegisterVC {
+    func setupUI() {
+        view.backgroundColor = .appBlack
+        view.addSubview(contentView)
+        contentView.addSubview(logoImageView)
+        contentView.addSubview(welcomeLabel)
+        contentView.addSubview(infoView)
+        infoView.addSubview(emailTextView)
+        infoView.addSubview(passwordTextView)
+        infoView.addSubview(repeatPasswordTextView)
+        view.addSubview(registerButton)
+        view.addSubview(iHaveAnAccountButton)
+    }
+    
+    func setupLayouts() {
+        contentView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(registerButton.snp.centerY)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(72)
+            make.size.equalTo(96)
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(infoView.snp.top).inset(-8)
+        }
+        
+        infoView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        emailTextView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        passwordTextView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.top.equalTo(emailTextView.snp.bottom).inset(-16)
+        }
+        
+        repeatPasswordTextView.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalToSuperview().inset(16)
+            make.top.equalTo(passwordTextView.snp.bottom).inset(-16)
+        }
+        
+        registerButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(45)
+            make.bottom.equalTo(iHaveAnAccountButton.snp.top).inset(-8)
+        }
+        
+        iHaveAnAccountButton.snp.makeConstraints { make in
+            make.horizontalEdges.greaterThanOrEqualToSuperview().inset(20)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+}
