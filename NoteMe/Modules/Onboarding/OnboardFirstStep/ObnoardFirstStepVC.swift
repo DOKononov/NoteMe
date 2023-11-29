b6874210d73bd1a4fb7ccce4784df0878b82e715
d.o.kononov <d.o.kononov@gmail.com>
2023-11-29 11:50:04 +0300

    OnboardFirstStepVC UI done
    finished OnboardFirstStepVC UI
    update Localizable

--- NoteMe/Modules/Onboarding/OnboardFirstStep/ObnoardFirstStepVC.swift
+++ NoteMe/Modules/Onboarding/OnboardFirstStep/ObnoardFirstStepVC.swift
@@ -16,6 +16,20 @@ final class OnboardFirstStepVC: UIViewController {
     
     private var viewModel: OnboardFirstStepViewModelProtocol
     
+    private lazy var contentView: UIView = .contentView()
+    private lazy var logoContainer: UIView = UIView()
+    private lazy var logoImageView: UIImageView = .init(image: .General.logo)
+    private lazy var titleLabel: UILabel = .titleLabel(.Onboarding.welcome)
+    private lazy var infoView: UIView = .infoView()
+    
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = .appFont.withSize(13)
+        label.text = .Onboarding.noteme_is_an_application_which_notify_you_about_everything
+        label.numberOfLines = 2
+        return label
+    }()
+    
     private lazy var nextButton: UIButton =
         .yellowRoundedButton(.Onboarding.next)
         .withAction(viewModel, #selector(OnboardFirstStepViewModelProtocol.nextDidTap))
@@ -37,16 +51,53 @@ final class OnboardFirstStepVC: UIViewController {
 
 //MARK: -UI
 private extension OnboardFirstStepVC {
-     func setupUI() {
-        view.backgroundColor = .appGray
+    func setupUI() {
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .appBlack
+        view.addSubview(contentView)
+        contentView.addSubview(logoContainer)
+        logoContainer.addSubview(logoImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(infoView)
+        infoView.addSubview(infoLabel)
         view.addSubview(nextButton)
     }
     
     private func setupConstraints() {
-        nextButton.snp.makeConstraints { make in
+        contentView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(nextButton.snp.centerY)
+        }
+        
+        logoContainer.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalToSuperview()
+            make.bottom.equalTo(titleLabel.snp.top)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.size.equalTo(96)
+            make.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(infoView.snp.top).inset(-8)
+        }
+        
+        infoView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
             make.centerY.equalToSuperview()
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(16)
+        }
+        
+        nextButton.snp.makeConstraints { make in
             make.horizontalEdges.equalToSuperview().inset(20)
             make.height.equalTo(45)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
     }
 }
--- NoteMe/Resource/Constants/Localizations/Localization+Onboarding.swift
+++ NoteMe/Resource/Constants/Localizations/Localization+Onboarding.swift
@@ -15,4 +15,12 @@ extension String.Onboarding {
     static var done: String {
         "done".localized
     }
+    
+    static var welcome: String {
+        "welcome".localized
+    }
+    
+    static var noteme_is_an_application_which_notify_you_about_everything: String {
+        "noteme_is_an_application_which_notify_you_about_everything".localized
+    }
 }
